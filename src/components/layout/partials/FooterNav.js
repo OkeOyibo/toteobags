02c49fa90bf42102f5b2b0ac87097767a7cab4f1
Severin Hasler <severinhasler@hotmail.com>
2021-11-16 16:29:48 +0100

    add useLocation hook and use it in return

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -1,38 +1,23 @@
-import React from 'react';
-import classNames from 'classnames';
-import { Link } from 'react-router-dom';
+import React from "react";
+import classNames from "classnames";
+import { Link, useLocation, useHistory } from "react-router-dom";
 
-const FooterNav = ({
-  className,
-  ...props
-}) => {
-
-  const classes = classNames(
-    'footer-nav',
-    className
-  );
-
+const FooterNav = ({ className, ...props }) => {
+  const classes = classNames("footer-nav", className);
+  const location = useLocation();
   return (
-    <nav
-      {...props}
-      className={classes}
-    >
+    <nav {...props} className={classes}>
       <ul className="list-reset">
-        <li>
-          <Link to="#0">Contact</Link>
-        </li>
-        <li>
-          <Link to="#0">About us</Link>
-        </li>
-        <li>
-          <Link to="#0">FAQ's</Link>
-        </li>
-        <li>
-          <Link to="#0">Support</Link>
-        </li>
+        {location.pathname == "/" ? (
+          <li>
+            <Link to="/contact">Contact us -></Link>
+          </li>
+        ) : (
+          <></>
+        )}
       </ul>
     </nav>
   );
-}
+};
 
 export default FooterNav;
